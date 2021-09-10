97b1e6b3e66def28ebbabf9ff9ff607f7532e5b3
Franco <franco@perfit.com.ar>
2021-09-10 16:22:40 -0300

    Version 9.0

--- backend/src/validator.js
+++ backend/src/validator.js
@@ -1,7 +1,7 @@
 async function validateAdmin(req, res, next) {
   const { headers } = req;
   try {
-    if(headers.admin){
+    if(headers.admin == 'true'){
         next();
     } else {
         throw new Error("Error")
